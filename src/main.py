ceb7f6e3e33c16176f7c9d5175ec66dcc2870b23
Leizzz69 <leif.eggenfellner@gmail.com>
2021-10-02 02:00:13 +0200

    print out the solved sudoku

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 import generate_grid
 import draw_grid
-# import solve
+import solve
 
 
 def main():
     grid = [[0 for _ in range(9)] for _ in range(9)]
-
     grid = generate_grid.generate(grid)
+
     draw_grid.draw(grid)
+
+    solve.solve_grid(grid)
+    for row in grid:
+        print(row, end="\n")
 
 
 if __name__ == "__main__":
